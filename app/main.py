64c5e2bef971027610a05e5215682625a9abd60d
erick <e.alejandro1705@gmail.com>
2025-06-23 23:19:55 -0500

    cambio en las importaciones de la API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import capas, reportes,usuarios
-from .DataBase.db import init_db
+from app.routers import capas, reportes,usuarios
+from app.DataBase.db import init_db
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
